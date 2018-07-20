e606cf2335746269fb0f7866848f8ddbcb2cd5cc
Ethan <940941188@qq.com>
2018-07-20 17:21:08 +0800

    fix 增加环境变量

--- container_start.sh
+++ container_start.sh
@@ -1,1 +1,14 @@
+config=/etc/nginx/conf.d/default.conf
+sed -i "s/http:\/\/elephantbi.com/${}/g" $config
+sed -i "s/http:\/\/m.elephantbi.com/${}/g" $config
+
+indexFile=/usr/share/nginx/html/index.html
+sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $indexFile
+serverFile=/usr/share/nginx/html/server.html
+sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $serverFile
+productFile=/usr/share/nginx/html/product.html
+sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $productFile
+aboutFile=/usr/share/nginx/html/about.html
+sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $aboutFile
+
 nginx -p /usr/share/nginx -g "daemon off;"
