a7d2401a3390cca91f937196fc23f464b37a01e0
Ethan <940941188@qq.com>
2018-07-20 17:28:28 +0800

    fix 环境变量 地址

--- container_start.sh
+++ container_start.sh
@@ -1,6 +1,6 @@
 config=/etc/nginx/conf.d/default.conf
 sed -i "s/http:\/\/elephantbi.com/${WEBSITE_URL}/g" $config
-sed -i "s/http:\/\/m.elephantbi.com/${WEBSITE_M_URL}/g" $config
+sed -i "s/http:\/\/m.elephantbi.com/${WEBSITE_MOBILE_URL}/g" $config
 
 indexFile=/usr/share/nginx/html/index.html
 sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $indexFile
