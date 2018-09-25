531adcd918c87170f17e173d89d1146f0c0f1971
akamuinsaner <15108387504@163.com>
2018-09-25 14:46:45 +0800

    FIX 文案

--- src/js/main.js
+++ src/js/main.js
@@ -559,7 +559,7 @@ const switchToGroup = (e) => {
     domainDescription.setAttribute('class', 'url-description')
     domainDescription.style.width = '300px'
     domainDescription.style.marginBottom = '35px'
-    domainDescription.innerText = '免验证邮箱域名的帐号可以直接注册加入团队，不需要管理员审核，团队创建后可在团队管理面板继续添加'
+    domainDescription.innerText = '团队成员使用免验证邮箱后缀注册，则不需要管理员审核，可直接进入团队。后缀示例：qq.com'
 
     domainField.appendChild(label)
     domainField.appendChild(domainItemWrapper)
