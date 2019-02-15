f5439f0c972abed1a0d31430b5575fcce32a17a9
akamuinsaner <15108387504@163.com>
2019-02-15 12:53:56 +0800

    FIX xydatatech替换

--- container_start.sh
+++ container_start.sh
@@ -9,19 +9,19 @@ indexFile=/usr/share/nginx/html/index.html
 sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $indexFile
 sed -i "s/DEMO_URL/${DEMO_URL}/g" $indexFile
 sed -i "s/VIDEO_URL/${VIDEO_URL}/g" $indexFile
-sed -i "s/DATATECH_URL/${XYDATATECH_URL}/g" $indexFile
+sed -i "s/https:\/\/www.xydatatech.com/${XYDATATECH_URL}/g" $indexFile
 serverFile=/usr/share/nginx/html/service.html
 sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $serverFile
 sed -i "s/DEMO_URL/${DEMO_URL}/g" $serverFile
-sed -i "s/DATATECH_URL/${XYDATATECH_URL}/g" $serverFile
+sed -i "s/https:\/\/www.xydatatech.com/${XYDATATECH_URL}/g" $serverFile
 demoFile=/usr/share/nginx/html/demo.html
 sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $demoFile
 sed -i "s/DEMO_URL/${DEMO_URL}/g" $demoFile
-sed -i "s/DATATECH_URL/${XYDATATECH_URL}/g" $demoFile
+sed -i "s/https:\/\/www.xydatatech.com/${XYDATATECH_URL}/g" $demoFile
 priceFile=/usr/share/nginx/html/price.html
 sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $priceFile
 sed -i "s/DEMO_URL/${DEMO_URL}/g" $priceFile
-sed -i "s/DATATECH_URL/${XYDATATECH_URL}/g" $priceFile
+sed -i "s/https:\/\/www.xydatatech.com/${XYDATATECH_URL}/g" $priceFile
 registerFile=/usr/share/nginx/html/register.html
 sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $registerFile
 sed -i "s/DEMO_URL/${DEMO_URL}/g" $registerFile
@@ -34,19 +34,19 @@ indexFileEn=/usr/share/nginx/html/en/index.html
 sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $indexFileEn
 sed -i "s/DEMO_URL/${DEMO_URL}/g" $indexFileEn
 sed -i "s/VIDEO_URL/${VIDEO_URL}/g" $indexFileEn
-sed -i "s/DATATECH_URL/${XYDATATECH_URL}/g" $indexFileEn
+sed -i "s/https:\/\/www.xydatatech.com/${XYDATATECH_URL}/g" $indexFileEn
 serverFileEn=/usr/share/nginx/html/en/service.html
 sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $serverFileEn
 sed -i "s/DEMO_URL/${DEMO_URL}/g" $serverFileEn
-sed -i "s/DATATECH_URL/${XYDATATECH_URL}/g" $serverFileEn
+sed -i "s/https:\/\/www.xydatatech.com/${XYDATATECH_URL}/g" $serverFileEn
 demoFileEn=/usr/share/nginx/html/en/demo.html
 sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $demoFileEn
 sed -i "s/DEMO_URL/${DEMO_URL}/g" $demoFileEn
-sed -i "s/DATATECH_URL/${XYDATATECH_URL}/g" $demoFileEn
+sed -i "s/https:\/\/www.xydatatech.com/${XYDATATECH_URL}/g" $demoFileEn
 priceFileEn=/usr/share/nginx/html/en/price.html
 sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $priceFileEn
 sed -i "s/DEMO_URL/${DEMO_URL}/g" $priceFileEn
-sed -i "s/DATATECH_URL/${XYDATATECH_URL}/g" $priceFileEn
+sed -i "s/https:\/\/www.xydatatech.com/${XYDATATECH_URL}/g" $priceFileEn
 registerFile=/usr/share/nginx/html/register.html
 sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $registerFile
 sed -i "s/DEMO_URL/${DEMO_URL}/g" $registerFile
@@ -59,19 +59,19 @@ indexFileHk=/usr/share/nginx/html/hk/index.html
 sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $indexFileHk
 sed -i "s/DEMO_URL/${DEMO_URL}/g" $indexFileHk
 sed -i "s/VIDEO_URL/${VIDEO_URL}/g" $indexFileHk
-sed -i "s/DATATECH_URL/${XYDATATECH_URL}/g" $indexFileHk
+sed -i "s/https:\/\/www.xydatatech.com/${XYDATATECH_URL}/g" $indexFileHk
 serverFileHk=/usr/share/nginx/html/hk/service.html
 sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $serverFileHk
 sed -i "s/DEMO_URL/${DEMO_URL}/g" $serverFileHk
-sed -i "s/DATATECH_URL/${XYDATATECH_URL}/g" $serverFileHk
+sed -i "s/https:\/\/www.xydatatech.com/${XYDATATECH_URL}/g" $serverFileHk
 demoFileHk=/usr/share/nginx/html/hk/demo.html
 sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $demoFileHk
 sed -i "s/DEMO_URL/${DEMO_URL}/g" $demoFileHk
-sed -i "s/DATATECH_URL/${XYDATATECH_URL}/g" $demoFileHk
+sed -i "s/https:\/\/www.xydatatech.com/${XYDATATECH_URL}/g" $demoFileHk
 priceFileHk=/usr/share/nginx/html/hk/price.html
 sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $priceFileHk
 sed -i "s/DEMO_URL/${DEMO_URL}/g" $priceFileHk
-sed -i "s/DATATECH_URL/${XYDATATECH_URL}/g" $priceFileHk
+sed -i "s/https:\/\/www.xydatatech.com/${XYDATATECH_URL}/g" $priceFileHk
 registerFile=/usr/share/nginx/html/register.html
 sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $registerFile
 sed -i "s/DEMO_URL/${DEMO_URL}/g" $registerFile
